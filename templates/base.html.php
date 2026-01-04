ab638596b5fd2a36f30ed6cc217d7ec0cb4491a6
kuba200333 <kuba.wiercinski2003@gmail.com>
2026-01-04 19:26:03 +0100

    add structure project

--- templates/base.html.php
+++ templates/base.html.php
@@ -6,11 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/assets/dist/style.min.css">
-    <title><?= $title ?? 'Custom Framework' ?></title>
+    <title><?= $title ?? 'Plusflix' ?></title>
 </head>
 <body <?= isset($bodyClass) ? "class='$bodyClass'" : '' ?>>
 <nav><?php require(__DIR__ . DIRECTORY_SEPARATOR . 'nav.html.php') ?></nav>
 <main><?= $main ?? null ?></main>
-<footer>&copy;<?= date('Y') ?> Custom Framework</footer>
+<footer>&copy;<?= date('Y') ?> Plusflix</footer>
 </body>
 </html>
--- templates/nav.html.php
+++ templates/nav.html.php
@@ -4,6 +4,5 @@
 ?>
 <ul>
     <li><a href="<?= $router->generatePath('') ?>">Home</a></li>
-    <li><a href="<?= $router->generatePath('post-index') ?>">Posts</a></li>
 </ul>
 <?php
