25fad7f757624fff4e72ead794154112ff2b3bb8
yr3dye <yaro.thirdeye@gmail.com>
2026-01-04 19:51:20 +0100

    (try2)

--- public/index.php
+++ public/index.php
@@ -8,6 +8,37 @@ $router = new \App\Service\Router();
 
 $action = $_REQUEST['action'] ?? null;
 switch ($action) {
+    case 'movie-index':
+    case null:
+        $controller = new \App\Controller\MovieController();
+        $view = $controller->indexAction();
+        break;
+        
+    case 'movie-show':
+        $controller = new \App\Controller\MovieController();
+        $view = $controller->showAction();
+        break;
+
+    case 'favorite-toggle':
+        $controller = new \App\Controller\FavoriteController();
+        $view = $controller->toggleAction();
+        break;
+
+    case 'favorite-index':
+        $controller = new \App\Controller\FavoriteController();
+        $view = $controller->indexAction();
+        break;
+
+    case 'rating-add':
+        $controller = new \App\Controller\RatingController();
+        $view = $controller->addAction();
+        break;
+
+    case 'comment-add':
+        $controller = new \App\Controller\CommentController();
+        $view = $controller->addAction();
+        break;
+	
     case 'info':
         $controller = new \App\Controller\InfoController();
         $view = $controller->infoAction();
